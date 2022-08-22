4d22400dafb9ad84612897497a5296b2c2a4cb41
zackhamin <ishaqhamin@gmail.com>
2022-08-22 08:37:34 +0100

    nextjs app scaffolding

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>An app to find the latest fairs and carnivals</p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EventPage() {
+  return <h1>My event</h1>;
+}
